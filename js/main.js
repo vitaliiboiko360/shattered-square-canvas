d56f1acdd880a772bebfca7bd6a1608e8091c281
vitalii boiko <vukelan@gmail.com>
2025-02-19 02:17:45 +0200

    compensate exclusive 1 from math random

--- js/main.js
+++ js/main.js
@@ -17,16 +17,16 @@
     const r2 = Math.random();
 
     if (r1 == 0) {
-      return { x: r2 * w + sx1, y: sy1 };
+      return { x: r2 * w + sx1 + 1, y: sy1 };
     }
     if (r1 == 2) {
-      return { x: r2 * w + sx1, y: sy2 };
+      return { x: r2 * w + sx1 + 1, y: sy2 };
     }
     if (r1 == 1) {
-      return { x: sx1, y: r2 * h + sy1 };
+      return { x: sx1, y: r2 * h + sy1 + 1 };
     }
     if (r1 == 3) {
-      return { x: sx2, y: r2 * h + sy1 };
+      return { x: sx2, y: r2 * h + sy1 + 1 };
     }
   };
 
@@ -37,6 +37,7 @@
       const { x: x1, y: y1 } = getRandomPointOnPerim();
       // console.log(`x1 == ${x1}`);
       // console.log(`y1 == ${y1}`);
+      ctx.beginPath();
       ctx.moveTo(x1, y1);
       let { x: x2, y: y2 } = getRandomPointOnPerim();
       while (x2 == x1 || y2 == y1) {
@@ -48,8 +49,9 @@
       // console.log(`y2 == ${y2}`);
       ctx.lineTo(x2, y2);
       ctx.stroke();
+      ctx.closePath();
     };
-
+    ctx.strokeStyle = '#00FF00';
     Array.from({ length: 20 }).forEach(() => {
       drawRandomLine();
     });
